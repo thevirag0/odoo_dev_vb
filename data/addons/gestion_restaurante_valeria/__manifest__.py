cf46a64540890255a3163b397e3555c83f8d6933
Valeria <valberroc@alu.edu.gva.es>
2026-02-03 11:19:48 +0100

    "setup completo"

--- data/addons/gestion_restaurante_valeria/__manifest__.py
+++ data/addons/gestion_restaurante_valeria/__manifest__.py
@@ -1,19 +1,19 @@
 {
     'name': "GestionRestauranteValeria",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Gestión del restaurante",
 
     'description': """
-Long description of module's purpose
+Módulo que servirá de plantilla para el proyecto.
     """,
 
-    'author': "My Company",
+    'author': "Valeria B.",
     'website': "https://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Tools',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -21,10 +21,13 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
