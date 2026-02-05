521822a4205366559d7190cb7a3da9443bb011e7
thevirag0 <ve.berrutti@gmail.com>
2026-02-05 23:35:11 +0100

    "Modelos actualizado"

--- data/addons/gestion_restaurante_valeria/models/models.py
+++ data/addons/gestion_restaurante_valeria/models/models.py
@@ -7,3 +7,16 @@ class Plato(models.Model):
 
     name = fields.Char(string="Nombre del Plato", required=True)
     description = fields.Text(string="Ingredientes/Descripción")
+    price = fields.Float(string="Precio", required=True)
+    preparation_time = fields.Integer(string="Tiempo de Preparación (minutos)", required=False, help="Tiempo estimado de preparación del plato")
+    available = fields.Boolean(string="Disponible", default="True", required=True, help="Indica si el plato puede ser preparado en este momento")
+    category = fields.Selection(
+        string="Categoría",
+        selection=[
+            ('entrante', 'Entrante'),
+            ('plato_principal', 'Plato Principal'),
+            ('postre', 'Postre'),
+            ('bebida', 'Bebida')
+        ],
+        default='plato_principal'
+    )
