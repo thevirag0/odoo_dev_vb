034490bb459b4b801f20a1a8aa75ae11bc2f3109
thevirag0 <ve.berrutti@gmail.com>
2026-02-05 23:15:09 +0100

    "Creación de modelo básico HECHA ✔ "

--- data/addons/gestion_restaurante_valeria/models/models.py
+++ data/addons/gestion_restaurante_valeria/models/models.py
@@ -1,15 +1,9 @@
 from odoo import models, fields, api
 
 
-class platos(models.Model):
-    _name = 'gestion_restaurante_valeria.platos'
-    _description = 'Modelo de Platos'
+class Plato(models.Model):
+    _name = 'gestion_restaurante_valeria.plato'
+    _description = 'Modelo para gestionar los platos del restaurante'
 
-    name = fields.Char()
-    value = fields.Char()
-
-#    @api.depends('value')
-#    def _value_pc(self):
-#        for record in self:
-#            record.value2 = float(record.value) / 100
-
+    name = fields.Char(string="Nombre del Plato", required=True)
+    description = fields.Text(string="Ingredientes/Descripción")
